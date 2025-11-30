a8c9f17dac45ec9e0e03c00933a6a194a12395e3
Brian Henry <BrianHenryIE@gmail.com>
2025-11-29 17:01:33 -0800

    phpcs

--- src/ColorLogger.php
+++ src/ColorLogger.php
@@ -58,7 +58,7 @@ class ColorLogger extends TestLogger
             default:
         }
         echo "{$message}\n";
-        if(function_exists('codecept_debug')) {
+        if (function_exists('codecept_debug')) {
             codecept_debug("{$message}\n");
         }
         ob_flush();
